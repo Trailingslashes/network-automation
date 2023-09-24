cfd8d96043bf37411c33c09759fd771d796ef67f
cphillips <cphillips@devnet.home>
2023-09-24 12:02:47 -0400

    Nested list should be seperated by colon

--- extra/file_processing.py
+++ extra/file_processing.py
@@ -1,12 +1,15 @@
 nested_list = []
 
 with open('devices.txt') as f:
-	lines = f.readlines()
+    lines = f.readlines()
 
 for line in lines[1:]:
-    # Remove trailing newlines and split the line based on comma
-    nested_list.append(line.strip().split(','))
+    # Remove trailing newlines and split the line based on a colon
+    nested_list.append(line.strip().split(':'))
+
 
 print(nested_list)
 
-f.close()
+# ping each device from the nested list
+for devices in nested_list:
+    print(f'pinging {devices[1]}')
