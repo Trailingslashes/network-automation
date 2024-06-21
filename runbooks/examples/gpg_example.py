739dac19c49271e75c0d41f950288cb1e915126f
trailingslashes <colephillips1@gmail.com>
2024-06-21 11:44:18 -0400

    Add gpg example

---
+++ runbooks/examples/gpg_example.py
@@ -0,0 +1,29 @@
+#  pip install python-gnupg
+#  gpg --gen-key , follow prompts
+# create a folder with a txt file with the following variables
+# DEFAULT_USERNAME=
+# DEFAULT_PASSWORD=
+# gpg --symmetric -o enc.env.gpg passwords
+# gpg --decrypt enc.env.gpg
+# flush gpg cache
+# echo RELOADAGENT | gpg-connect-agent
+import os
+
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yml")
+
+
+nr.inventory.defaults.username = os.environ["DEFAULT_USERNAME"]
+nr.inventory.defaults.password = os.environ["DEFAULT_PASSWORD"]
+
+
+def cred_test(task):
+    task.run(send_command, command="show version")
+
+
+results = nr.run(task=cred_test)
+
+print_result(results)
